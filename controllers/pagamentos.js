faa45b1884882e6740cfa840e836a1c475193eb1
joao.leite <joao.leite@restoque.com.br>
2020-04-22 19:22:29 -0300

    Criando os pagamentos

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,7 +1,19 @@
-module.exports = function(app){
- app.get('/pagamentos', function(req, res){
-    console.log('Recebida requisicao de pagamento.')
-      res.send('OK.');
+module.exports = function (app) {
+          app.get('/pagamentos', function (req, res) {
+                    console.log('Recebida requisicao de pagamento.')
+                    res.send('OK.');
+          });
+
+          app.post('/pagamentos/pagamento', function (req, res) {
+                    var pagamento = req.body;
+                    console.log('procesando uma requisicao de um novo pagto');
+
+                    pagamento.status = 'CRIADO';
+                    pagamento.data =new Date;
+
+                    res.send('OK.');
+
+
           });
 
 }
