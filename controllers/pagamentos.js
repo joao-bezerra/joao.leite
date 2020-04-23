b93ecfb045022ff73ef4efb81ffee9d31aff4902
joao.leite <joao.leite@restoque.com.br>
2020-04-23 19:21:05 -0300

    Validando o insert no Mysql

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -5,15 +5,41 @@ module.exports = function (app) {
           });
 
           app.post('/pagamentos/pagamento', function (req, res) {
+
+                    req.assert("forma_de_pagamento",
+                     "Forma de pagamento eh obrigatorio").notEmpty();
+                    req.assert("valor",
+                     "Valor eh obrigatorio e deve ser um decimal")
+                     .notEmpty().isFloat();
+
+                     var erros = req.validationErrors();
+
+                     if (erros){
+                        console.log("Erros de validação encontrados");
+                        res.status(400).send(erros);
+                        return;
+                     }
+
                     var pagamento = req.body;
                     console.log('procesando uma requisicao de um novo pagto');
 
                     pagamento.status = 'CRIADO';
-                    pagamento.data =new Date;
+                    pagamento.data = new Date;
 
-                    res.send('OK.');
+                    var connection = app.persistencia.connectionFactory();
+                    var pagamentoDao = new app.persistencia.PagamentoDao(connection);
 
-
-          });
+                    pagamentoDao.salva(pagamento, function(erro, resultado){
+                        if (erro){
+                           console.log('Erro ao inserir no banco:'+ erro);       
+                           res.status(400).send(erro);       
+                        } else {
+                        console.log('pagamento criado');
+                        res.json(pagamento); 
+                        }      
+                             
+                    });
+         
+                   });
 
 }
